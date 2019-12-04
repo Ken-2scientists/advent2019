d54b812b05f5a8d2c4ec2c8fc24e6c5286575484
Ken Smith <ken.smith@2scientists.net>
2019-12-03 21:11:03 -0700

    Solution to Day 03 Part 2 (sans cleanup)

--- src/advent2019/day03.clj
+++ src/advent2019/day03.clj
@@ -39,9 +39,49 @@
         path2 (set (trace-wire-path wire2))]
     (clojure.set/intersection path1 path2)))
 
+(defn metropolitan-dist
+  [[x y]]
+  (+ (Math/abs x) (Math/abs y)))
+
 (defn closest-intersection-dist
   [instructions]
   (->> (intersections instructions)
+       (map metropolitan-dist)
        sort
-       second ; we take second because [0 0] is in the set
-       (reduce +)))
+       ; we take second because [0 0] is in the set
+       second))
+
+(defn day03-part1-soln
+  []
+  (closest-intersection-dist day03-input))
+
+; Slightly convoluted. I want a mapping from a point
+; to the shortest distance to that point. Because 
+; later values clobber earlier ones, I traverse the
+; path backwards so the shortest distance to a single
+; point remains in the map at the end
+(defn shortest-distance-to-point
+  [path]
+  (let [len (count path)]
+    (zipmap (reverse path) (reverse (range len)))))
+
+
+(defn distances-to-intersections
+  [[wire1 wire2]]
+  (let [path1 (trace-wire-path wire1)
+        path2 (trace-wire-path wire2)
+        path1-dists (shortest-distance-to-point path1)
+        path2-dists (shortest-distance-to-point path2)
+        ints (clojure.set/intersection (set path1) (set path2))]
+    (map (fn [pos] [pos (+ (get path1-dists pos) (get path2-dists pos))]) ints)))
+
+(defn shortest-steps-to-intersection
+  [[wire1 wire2]]
+  (->> (distances-to-intersections [wire1 wire2])
+       (sort-by second)
+       second
+       second))
+
+(defn day03-part2-soln
+  []
+  (shortest-steps-to-intersection day03-input))
--- test/advent2019/day03_test.clj
+++ test/advent2019/day03_test.clj
@@ -23,18 +23,27 @@
 
 (def input1 '[["R8","U5","L5","D3"]
               ["U7","R6","D4","L4"]])
-(def output1 6)
+(def dist1 6)
+(def steps1 30)
 
 (def input2 '[["R75","D30","R83","U83","L12","D49","R71","U7","L72"]
               ["U62","R66","U55","R34","D71","R55","D58","R83"]])
-(def output2 159)
+(def dist2 159)
+(def steps2 610)
 
 (def input3 '[["R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"]
               ["U98","R91","D20","R16","D67","R40","U7","R15","U6","R7"]])
-(def output3 135)
+(def dist3 135)
+(def steps3 410)
 
 (deftest part1-test
   (testing "Find lower-leftmost intersection of the wire paths"
-    (is (= output1 (closest-intersection-dist input1)))
-    (is (= output2 (closest-intersection-dist input2)))
-    (is (= output3 (closest-intersection-dist input3)))))
+    (is (= dist1 (closest-intersection-dist input1)))
+    (is (= dist2 (closest-intersection-dist input2)))
+    (is (= dist3 (closest-intersection-dist input3)))))
+
+(deftest part2-test
+  (testing "Find fewest number of steps to first intersection of the wire paths"
+    (is (= steps1 (shortest-steps-to-intersection input1)))
+    (is (= steps2 (shortest-steps-to-intersection input2)))
+    (is (= steps3 (shortest-steps-to-intersection input3)))))
