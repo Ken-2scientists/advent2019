d065782dd32baadf2c8363b8414d038defeb7b2b
Ken Smith <ken.smith@2scientists.net>
2019-12-06 06:55:51 -0700

    Day 06 part 2 soln

--- src/advent2019/day06.clj
+++ src/advent2019/day06.clj
@@ -43,3 +43,38 @@
        (map (partial orbit-chain orbits))
        (map orbit-length)
        (reduce +)))
+
+(def sample-data2
+  {"B" "COM"
+   "C" "B"
+   "D" "C"
+   "E" "D"
+   "F" "E"
+   "G" "B"
+   "H" "G"
+   "I" "D"
+   "J" "E"
+   "K" "J"
+   "L" "K"
+   "YOU" "K"
+   "SAN" "I"})
+
+(defn first-common-ancestor
+  [orbits objA objB]
+  (let [chainA (orbit-chain orbits objA)
+        chainB (orbit-chain orbits objB)
+        common (map vector (reverse chainA) (reverse chainB))
+        first-delta (first (filter (fn [[a b]] (not= a b)) common))]
+    (get orbits (first first-delta))))
+
+(defn distance-to-ancestor
+  [orbits des anc]
+  (let [chain (orbit-chain orbits des)]
+    (count (take-while (partial not= anc) chain))))
+
+(defn orbit-transfers
+  [orbits objA objB]
+  (let [ancestor (first-common-ancestor orbits objA objB)]
+    (- (+ (distance-to-ancestor orbits objA ancestor)
+          (distance-to-ancestor orbits objB ancestor))
+       2)))
