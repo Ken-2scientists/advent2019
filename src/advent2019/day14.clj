e8e977a9621c28698d722d313cde098c40c8797a
Ken Smith <ken.smith@2scientists.net>
2019-12-21 14:25:01 -0700

    Made progress, but not enough

--- src/advent2019/day14.clj
+++ src/advent2019/day14.clj
@@ -13,7 +13,7 @@
   [line]
   (let [[lhs rhs] (str/split line #" \=\> ")
         lhs-split (str/split lhs #", ")
-        components (map parse-component lhs-split)
+        components (mapcat parse-component lhs-split)
         [chem qty] (parse-component rhs)]
     {chem {:min-qty qty :comps components}}))
 
@@ -33,6 +33,15 @@
               "7 A, 1 C => 1 D"
               "7 A, 1 D => 1 E"
               "7 A, 1 E => 1 FUEL"]))
+
+(def d14-s2 (reactions
+             ["9 ORE => 2 A"
+              "8 ORE => 3 B"
+              "7 ORE => 5 C"
+              "3 A, 4 B => 1 AB"
+              "5 B, 7 C => 1 BC"
+              "4 C, 1 A => 1 CA"
+              "2 AB, 3 BC, 4 CA => 1 FUEL"]))
 
 (def d14-s3 (reactions
              ["157 ORE => 5 NZVS"
@@ -96,12 +105,54 @@
   (let [{:keys [min-qty comps]} (get reactions chemical)]
     (map (partial fulfill-requirement reactions inventory claimed min-qty) comps)))
 
+(defn multiply
+  [factor ingredients]
+  (mapcat (fn [[chem qty]] [chem (* factor qty)]) (partition 2 ingredients)))
+
+(defn consume
+  [inventory used [chemical qty]]
+  (swap! used update chemical #(+ % qty))
+  (swap! inventory update chemical #(- % qty)))
+
+(defn take-or-make
+  [recipes inventory used chemical qty]
+  (let [{:keys [min-qty comps]} (get recipes chemical)
+        available (get @inventory chemical)
+        multiplier (int (Math/ceil (/ qty min-qty)))
+        needs (multiply multiplier comps)]
+    (if (> available qty)
+      (consume inventory used [chemical qty])
+      (do
+        (doseq [comp (partition 2 needs)]
+          (consume inventory used comp))))))
+
+(defn order-if-needed
+  [recipes inventory used [chemical qty]]
+  (if (= :ORE chemical)
+    (swap! inventory update chemical #(+ % qty))
+    (let [{:keys [min-qty comps]} (get recipes chemical)
+          available (get @inventory chemical)
+          order-amount (int (Math/ceil (/ (- qty available) min-qty)))
+          multiplier (int (Math/ceil (/ qty min-qty)))
+          orders (multiply order-amount comps)]
+      (doseq [[x q] (partition 2 orders)]
+        (when (> q (get @inventory x))
+          (order-if-needed recipes inventory used [x q]))
+        (consume inventory used [x q]))
+      (swap! inventory update chemical #(+ % (* multiplier min-qty))))))
+
 (defn empty-state
-  [reactions]
-  (zipmap (conj (keys reactions) :ORE) (repeat 0)))
+  [recipes]
+  (zipmap (conj (keys recipes) :ORE) (repeat 0)))
+
+(defn ingredients-used
+  [recipes chemical qty]
+  (let [inventory (atom (empty-state recipes))
+        used (atom (empty-state recipes))]
+    (order-if-needed recipes inventory used [chemical qty])
+    {:remaining @inventory
+     :consumed @used}))
 
 (defn ore-amount
-  [reactions]
-  (let [inventory (atom (empty-state reactions))
-        claimed (atom (empty-state reactions))]
-    (fulfill-requirements reactions inventory claimed :C)))
+  [recipes]
+  (get-in (ingredients-used recipes :FUEL 1) [:consumed :ORE]))
