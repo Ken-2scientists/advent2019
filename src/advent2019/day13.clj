142648404a27fba6ba30e39fdcc970232ca00c3f
Ken Smith <ken.smith@2scientists.net>
2019-12-21 11:14:44 -0700

    Day 13 part 2 solution
    Self-playing breakout game

--- src/advent2019/day13.clj
+++ src/advent2019/day13.clj
@@ -35,12 +35,28 @@
     (scr/put-string screen 50 0 (str "Score: " v))
     (scr/put-string screen x y (val->str v))))
 
+(defn location-of
+  [updates item]
+  (first (filter #(= item (last %)) updates)))
+
+(defn move-paddle
+  [ball-loc paddle-loc]
+  (if (or (nil? ball-loc) (nil? paddle-loc))
+    0
+    (cond
+      (< ball-loc paddle-loc) -1
+      (> ball-loc paddle-loc) 1
+      (= ball-loc paddle-loc) 0)))
+
 (defn update-screen
-  [screen in out]
-  (let [updates (partition 3 (s/stream->seq out 50))]
+  [screen in out paddle-loc]
+  (let [updates (partition 3 (s/stream->seq out 5))
+        ball-loc (first (location-of updates 4))
+        move (move-paddle ball-loc @paddle-loc)]
+    (swap! paddle-loc (partial + move))
     (doseq [update updates] (process-update screen update))
     (scr/redraw screen)
-    (s/put! in (input->str (scr/get-key-blocking screen)))))
+    (s/put! in move)))
 
 (defn breakout
   []
@@ -48,11 +64,12 @@
         out (s/stream)
         screen (scr/get-screen :swing)
         code (assoc day13-input 0 2)
-        program (d/future (intcode/intcode-ex-async code in out))]
+        program (d/future (intcode/intcode-ex-async code in out))
+        paddle-loc (atom 20)]
     (scr/start screen)
     (scr/put-string screen 50 0 (str "Score: 0"))
     (while (not (realized? program))
-      (update-screen screen in out))
+      (update-screen screen in out paddle-loc))
     @program))
 
 (defn print-board
